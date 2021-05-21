1177699a13d7783da40512a7dd09eb7d6cc1acae
Jean Pierre <frankjeanpierregs@ufps.edu.co>
2021-05-20 21:23:39 -0500

    fix bugs

--- src/controllers/cuerpoTecnico.controller.js
+++ src/controllers/cuerpoTecnico.controller.js
@@ -1,34 +1,38 @@
 const { CuerpoTecnico } = require("../database/database").models;
 
 module.exports = {
-    async guardar(cuerpoTecnico) {
-        let cuerpoTecnicoVO = null;
-        try {
-            cuerpoTecnicoVO = await CuerpoTecnico.create(cuerpoTecnico);
-        } catch (error) {
-            console.log(error);
-        }
-        return cuerpoTecnicoVO;
-    },
-    async actualizar(cuerpoTecnico) {
-        const id = cuerpoTecnico.id;
-        let cuerpoTecnicoVO = null;
-        try {
-            cuerpoTecnicoVO = await CuerpoTecnico.findByPk(id);
-            cuerpoTecnicoVO = cuerpoTecnico;
-            cuerpoTecnicoVO.save();
-        } catch (error) {
-            console.log(error);
-        }
-        return cuerpoTecnicoVO;
-    },
-    async listarPorEquipo(id_equipo) {
-		let cuerposTecnicosVO = null;
+	async guardar(cuerpoTecnico) {
+		let cuerpoTecnicoVO = null;
 		try {
-			cuerposTecnicosVO = await CuerpoTecnico.findAll({where:id_equipo});
+			cuerpoTecnicoVO = await CuerpoTecnico.create(cuerpoTecnico);
 		} catch (error) {
 			console.log(error);
 		}
-		return cuerposTecnicosVO;
+		return cuerpoTecnicoVO;
+	},
+	async actualizar(cuerpoTecnico) {
+		const id = cuerpoTecnico.id;
+		let cuerpoTecnicoVO = null;
+		try {
+			cuerpoTecnicoVO = await CuerpoTecnico.findByPk(id);
+			for (const key in cuerpoTecnico) {
+				cuerpoTecnicoVO[key] = cuerpoTecnico[key];
+			}
+			cuerpoTecnicoVO.save();
+		} catch (error) {
+			console.log(error);
+		}
+		return cuerpoTecnicoVO;
+	},
+	async listarPorEquipo(id_equipo) {
+		let listaCuerpoTecnico = null;
+		try {
+			listaCuerpoTecnico = await CuerpoTecnico.findAll({
+				where: { id_equipo },
+			});
+		} catch (error) {
+			console.log(error);
+		}
+		return listaCuerpoTecnico;
 	},
 };
--- src/routes/cuerpoTecnico.routes.js
+++ src/routes/cuerpoTecnico.routes.js
@@ -3,6 +3,8 @@ const cuerpoTecnicoService = require("../services/cuerpoTecnico.service");
 
 router.route("/guardar").post(cuerpoTecnicoService.agregar);
 router.route("/guardar").put(cuerpoTecnicoService.actualizar);
-router.route("/listar_por_equipo/:id_equipo").get(cuerpoTecnicoService.listarEquipo);
+router
+	.route("/listar_por_equipo/:id_equipo")
+	.get(cuerpoTecnicoService.listarPorEquipo);
 
 module.exports = router;
