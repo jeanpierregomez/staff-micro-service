fa862fc7d54c254cd48f4ed98589204d47b3d648
Jean Pierre <frankjeanpierregs@ufps.edu.co>
2021-05-20 22:45:30 -0500

    get por id y eliminar integrantes

--- src/controllers/integranteCuerpoTecnico.controller.js
+++ src/controllers/integranteCuerpoTecnico.controller.js
@@ -35,4 +35,25 @@ module.exports = {
 		}
 		return listaIntegrante;
 	},
+	async getPorId(id) {
+		let integranteVO = null;
+		try {
+			integranteVO = await IntegranteCuerpoTecnico.findByPk(id);
+		} catch (error) {
+			console.log(error);
+		}
+		return integranteVO;
+	},
+	async eliminar(id) {
+		let integranteVO = null;
+		try {
+			integranteVO = await IntegranteCuerpoTecnico.findByPk(id);
+			if (integranteVO) {
+				integranteVO = await integranteVO.destroy();
+			}
+		} catch (error) {
+			console.log(error);
+		}
+		return integranteVO;
+	},
 };
